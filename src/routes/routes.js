ca4950811af048f97cbce5bbc72642d5873c0559
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-09-30 03:01:41 +0800

    Add content to sample files for handlebars

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,17 @@
 const router = require("express").Router()
 
 router.get("/", (req, res) => {
-    res.send("HELLO!")
+    res.render("index", {
+        title: "HomePage",
+        css: "index.css"
+    })
+})
+
+router.use((req, res) => {
+    res.render("error", {
+        title: "Page Not Found",
+        css: "index.css"
+    })
 })
 
 module.exports = router
