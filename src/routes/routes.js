5751f4b3e77a79f6606293f44a0ecc22e4fb5d6b
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-10-01 02:47:32 +0800

    To fix: Add dark underline to current tab

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,36 @@ const router = require("express").Router()
 
 router.get("/", (req, res) => {
     res.render("index", {
+        title: "NewJeans"
+    })
+})
+
+router.get("/members", (req, res) => {
+    res.render("index", {
+        title: "Members"
+    })
+})
+
+router.get("/discography", (req, res) => {
+    res.render("index", {
+        title: "Discography"
+    })
+})
+
+router.get("/awards", (req, res) => {
+    res.render("index", {
+        title: "Awards and Nominations",
+    })
+})
+
+router.get("/performances", (req, res) => {
+    res.render("index", {
+        title: "Performances",
+    })
+})
+
+router.get("/gallery", (req, res) => {
+    res.render("index", {
         title: "NewJeans",
     })
 })
