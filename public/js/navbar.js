9768504736db57a409e77cbc4fb1e2aead95f41f
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-10-01 16:19:21 +0800

    Complete NavBar mobile responsive feat

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,25 +1,46 @@
 // click action listeners for navbar buttons
 $(".navbar-logo").click(() => { window.location.href = "/" })
-$("#home-button").click(() => { window.location.href = "/" })
-$("#members-button").click(() => { window.location.href = "/members" })
-$("#discography-button").click(() => { window.location.href = "/discography" })
-$("#awards-button").click(() => { window.location.href = "/awards" })
-$("#performances-button").click(() => { window.location.href = "/performances" })
-$("#gallery-button").click(() => { window.location.href = "/gallery" })
+$("#home-button-1").click(() => { window.location.href = "/" })
+$("#members-button-1").click(() => { window.location.href = "/members" })
+$("#discography-button-1").click(() => { window.location.href = "/discography" })
+$("#awards-button-1").click(() => { window.location.href = "/awards" })
+$("#performances-button-1").click(() => { window.location.href = "/performances" })
+$("#gallery-button-1").click(() => { window.location.href = "/gallery" })
+
+$("#home-button-2").click(() => { window.location.href = "/" })
+$("#members-button-2").click(() => { window.location.href = "/members" })
+$("#discography-button-2").click(() => { window.location.href = "/discography" })
+$("#awards-button-2").click(() => { window.location.href = "/awards" })
+$("#performances-button-2").click(() => { window.location.href = "/performances" })
+$("#gallery-button-2").click(() => { window.location.href = "/gallery" })
 
 // hover event listener for navbar-buttons
-$(".navbar-button").mouseenter((e) => {
+$(".navbar-button3").mouseenter((e) => {
     const element_id = e.currentTarget.closest('.navbar-button').getAttribute("id")
     $("#" + element_id).css("transition-duration", "1s")
     $("#" + element_id).css("-webkit-transition-duration", "1s")
     $("#" + element_id).css("font-size", "18px")
 })
 
-$(".navbar-button").mouseleave((e) => {
+$(".navbar-button3").mouseleave((e) => {
     const element_id = e.currentTarget.closest('.navbar-button').getAttribute("id")
     $("#" + element_id).css("transition-duration", "1s")
     $("#" + element_id).css("-webkit-transition-duration", "1s")
     $("#" + element_id).css("font-size", "15px")
+})
+
+$(".navbar-button2").mouseenter((e) => {
+    const element_id = e.currentTarget.closest('.navbar-button').getAttribute("id")
+    $("#" + element_id).css("transition-duration", "1s")
+    $("#" + element_id).css("-webkit-transition-duration", "1s")
+    $("#" + element_id).css("font-size", "24px")
+})
+
+$(".navbar-button2").mouseleave((e) => {
+    const element_id = e.currentTarget.closest('.navbar-button').getAttribute("id")
+    $("#" + element_id).css("transition-duration", "1s")
+    $("#" + element_id).css("-webkit-transition-duration", "1s")
+    $("#" + element_id).css("font-size", "18px")
 })
 
 // click event listener for hamburger
@@ -38,3 +59,10 @@ function adjust_window() {
     }
 }
 window.onresize = adjust_window
+
+// click event listener for menu exit button
+$(".exit-button").click(() => {
+    $(".menu-modal").css("opacity", "0")
+    $(".navbar-panel").css("display", "flex")
+    $(".content").css("display", "flex")
+})
