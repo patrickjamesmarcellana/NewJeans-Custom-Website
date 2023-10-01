4932ede5309a6e8fa06b7c11b7dc90d991de30f5
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-10-01 12:53:57 +0800

    Add hamburger click event

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -7,17 +7,24 @@ $("#awards-button").click(() => { window.location.href = "/awards" })
 $("#performances-button").click(() => { window.location.href = "/performances" })
 $("#gallery-button").click(() => { window.location.href = "/gallery" })
 
-// hover action listener for navbar-buttons
+// hover event listener for navbar-buttons
 $(".navbar-button").mouseenter((e) => {
     const element_id = e.currentTarget.closest('.navbar-button').getAttribute("id")
     $("#" + element_id).css("transition-duration", "1s")
-    $("#" + element_id).closest('.navbar-button').css("-webkit-transition-duration", "1s")
-    $("#" + element_id).closest('.navbar-button').css("font-size", "18px")
+    $("#" + element_id).css("-webkit-transition-duration", "1s")
+    $("#" + element_id).css("font-size", "18px")
 })
 
 $(".navbar-button").mouseleave((e) => {
     const element_id = e.currentTarget.closest('.navbar-button').getAttribute("id")
     $("#" + element_id).css("transition-duration", "1s")
-    $("#" + element_id).closest('.navbar-button').css("-webkit-transition-duration", "1s")
-    $("#" + element_id).closest('.navbar-button').css("font-size", "15px")
+    $("#" + element_id).css("-webkit-transition-duration", "1s")
+    $("#" + element_id).css("font-size", "15px")
+})
+
+// click event listener for hamburger
+$(".navbar-hamburger").click(() => {
+    $(".menu-modal").css("display", "block")
+    $(".navbar-panel").css("display", "none")
+    $(".content").css("display", "none")
 })
