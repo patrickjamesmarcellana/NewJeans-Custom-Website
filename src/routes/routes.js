3367e623ab1781f045c995eb07976efef1a2a645
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-10-01 10:09:54 +0800

    Add bold effect to current tab

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,37 +2,85 @@ const router = require("express").Router()
 
 router.get("/", (req, res) => {
     res.render("index", {
-        title: "NewJeans"
+        title: "NewJeans",
+        current_page: {
+            home: true,
+            members: false,
+            discography: false,
+            awards: false,
+            performances: false,
+            gallery: false
+        }
     })
 })
 
 router.get("/members", (req, res) => {
     res.render("index", {
-        title: "Members"
+        title: "Members",
+        current_page: {
+            home: false,
+            members: true,
+            discography: false,
+            awards: false,
+            performances: false,
+            gallery: false
+        }
     })
 })
 
 router.get("/discography", (req, res) => {
     res.render("index", {
-        title: "Discography"
+        title: "Discography",
+        current_page: {
+            home: false,
+            members: false,
+            discography: true,
+            awards: false,
+            performances: false,
+            gallery: false
+        }
     })
 })
 
 router.get("/awards", (req, res) => {
     res.render("index", {
         title: "Awards and Nominations",
+        current_page: {
+            home: false,
+            members: false,
+            discography: false,
+            awards: true,
+            performances: false,
+            gallery: false
+        }
     })
 })
 
 router.get("/performances", (req, res) => {
     res.render("index", {
         title: "Performances",
+        current_page: {
+            home: false,
+            members: false,
+            discography: false,
+            awards: false,
+            performances: true,
+            gallery: false
+        }
     })
 })
 
 router.get("/gallery", (req, res) => {
     res.render("index", {
         title: "NewJeans",
+        current_page: {
+            home: false,
+            members: false,
+            discography: false,
+            awards: false,
+            performances: false,
+            gallery: true
+        }
     })
 })
 
