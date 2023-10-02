adc46de064a06f1cd3025759dc0ecccade295370
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-10-03 03:30:22 +0800

    Added enlarging effect on member photos

--- public/js/index.js
+++ public/js/index.js
@@ -18,6 +18,20 @@ const second_photo = {
     already_seen: false
 }
 
+const members_header = {
+    element: document.querySelector(".members-panel-header"),
+    already_seen: false
+}
+
+const members_elements = document.querySelectorAll(".member-photo")
+let members_arr = []
+for(let x = 0; x < 5; x++) {
+    members_arr.push({
+        element: members_elements[x],
+        already_seen: false
+    })
+}
+
 function apply_animation(element, animation, settings) {
     const bounding = element.element.getBoundingClientRect();
     if (!element.already_seen && bounding.top + (bounding.height / 2) > 0 && bounding.left + (bounding.width/2) > 0 && bounding.left + (bounding.width/2) < (window.innerWidth || document.documentElement.clientWidth) && bounding.top + (bounding.height / 2) < (window.innerHeight || document.documentElement.clientHeight)) {
@@ -27,22 +41,65 @@ function apply_animation(element, animation, settings) {
     }
 }
 
-apply_animation(first_photo, { transform: ['scale(2)', 'scale(1)'] },
+apply_animation(first_photo, { transform: ['scale(2)', 'scale(1)'], opacity: ['0', '1'] },
 { duration: 1000, fill: 'forwards', easing: 'ease-out' })
-apply_animation(header_element, { transform: ['translateX(-400%)', 'translateX(0)'] }, 
-{ duration: 500, fill: 'forwards', easing: 'ease-out' })
-apply_animation(content_element, { transform: ['translateX(400%)', 'translateX(0)']}, 
-{ duration: 500, fill: 'forwards', easing: 'ease-out' })
 
 
-$(window).scroll((e) => {
-    apply_animation(first_photo, { transform: ['scale(2)', 'scale(1)'] },
+$(window).on('scroll', () => {
+    apply_animation(first_photo, { transform: ['scale(2)', 'scale(1)'], opacity: ['0', '1'] },
     { duration: 1000, fill: 'forwards', easing: 'ease-out' })
     apply_animation(header_element, { transform: ['translateX(-400%)', 'translateX(0)'] }, 
     { duration: 500, fill: 'forwards', easing: 'ease-out' })
     apply_animation(content_element, { transform: ['translateX(400%)', 'translateX(0)']}, 
     { duration: 500, fill: 'forwards', easing: 'ease-out' })
     apply_animation(second_photo, { opacity: ['0', '1'] },
-    { duration: 1500, fill: 'forwards', easing: 'ease-out' })
+    { duration: 1000, fill: 'forwards', easing: 'ease-out' })
 
+    /* Members Part Animations */
+    apply_animation(members_header, { transform: ['translateX(400%)', 'translateX(0)']}, 
+    { duration: 500, fill: 'forwards', easing: 'ease-out' })
+    for(let x = 0; x < 5; x++) {
+        apply_animation(members_arr[x], { transform: ['translateY(-30%)', 'translateY(0)'], opacity: ['0', '1']}, 
+        { duration: 1500, fill: 'forwards', easing: 'ease-out' })
+    }
 })
+
+/* Members Photo Hover Effect */
+$(".member").mouseenter((e) => {
+    const element_id = e.currentTarget.closest('.member').getAttribute("id")
+    $("#" + element_id).css("transition-duration", "1s")
+    $("#" + element_id).css("-webkit-transition-duration", "1s")
+    $("#" + element_id).css("transform", "scale(1.3)")
+    $("#" + element_id).css("z-index", "10")
+    $("#" + element_id).css("filter", "drop-shadow(4px 4px 8px #3C3C3C)")
+})
+
+$(".member").mouseleave((e) => {
+    const element_id = e.currentTarget.closest('.member').getAttribute("id")
+    $("#" + element_id).css("transition-duration", "1s")
+    $("#" + element_id).css("-webkit-transition-duration", "1s")
+    $("#" + element_id).css("transform", "scale(1)")
+    $("#" + element_id).css("z-index", "0")
+    $("#" + element_id).css("filter", "none")
+})
+
+/* Storing user's device details in a variable*/
+let details = navigator.userAgent;
+  
+/* Creating a regular expression 
+containing some mobile devices keywords 
+to search it in details string*/
+let regexp = /android|iphone|kindle|ipad/i;
+  
+/* Using test() method to search regexp in details
+it returns boolean value*/
+let isMobileDevice = regexp.test(details);
+  
+if (isMobileDevice) {
+    $(".about-panel-header").css("padding-right", "0")
+    $(".about-panel-content").css("padding-right", "0")
+    $(".members-panel").css("padding-right", "0")
+    $(".members-panel-header").css("padding-right", "0")
+    $(".navbar-panel").css("padding-right", "0")
+    $(".menu-modal").css("padding-right", "0")
+}
