20a15660989a3e899329cd3d02a497c4a6b55e13
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-10-02 11:25:00 +0800

    Add photo 2

--- public/js/index.js
+++ public/js/index.js
@@ -12,25 +12,14 @@ const content_element = {
 }
 
 function apply_animation(element, animation, settings) {
-    const bounding_1 = element.element.getBoundingClientRect();
-    if (!element.already_seen && bounding_1.top >= 0 && bounding_1.left >= 0 && bounding_1.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding_1.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+    const bounding = element.element.getBoundingClientRect();
+    if (!element.already_seen && bounding.top + (bounding.height / 2) > 0 && bounding.left + (bounding.width/2) > 0 && bounding.left + (bounding.width/2) < (window.innerWidth || document.documentElement.clientWidth) && bounding.top + (bounding.height / 2) < (window.innerHeight || document.documentElement.clientHeight)) {
         element.element.animate(animation, settings)
         element.already_seen = true;
     }
 }
 
 $(window).scroll((e) => {
-    const bounding_1 = header_element.element.getBoundingClientRect();
-    if (!header_element.already_seen && bounding_1.top >= 0 && bounding_1.left >= 0 && bounding_1.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding_1.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
-        header.animate({
-            transform: ['translateX(-400%)', 'translateX(0)'],
-        }, {
-            duration: 500,
-            fill: 'forwards',
-            easing: 'ease-out',
-        });
-        header_element.already_seen = true;
-    }
     apply_animation(header_element, { transform: ['translateX(-400%)', 'translateX(0)'] }, 
     { duration: 500, fill: 'forwards', easing: 'ease-out' })
     apply_animation(content_element, { transform: ['translateX(400%)', 'translateX(0)']}, 
